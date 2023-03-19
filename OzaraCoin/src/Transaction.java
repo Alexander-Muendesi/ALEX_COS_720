e5a46ed37c64c9fb091c27c83445e316126cd06f
Alexander-Muendesi <u20426918@tuks.co.za>
2023-03-19 14:51:24 +0200

    ✨ Added methods and attributes to Transaction class

--- OzaraCoin/src/Transaction.java
+++ OzaraCoin/src/Transaction.java
@@ -2,4 +2,22 @@ public class Transaction {
     private String sender;
     private String receiver;
     private double amount;
+
+    public Transaction(String sender, String receiver, double amount){
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+    }
+
+    public String getSender(){
+        return this.sender;
+    }
+
+    public String receiver(){
+        return this.receiver;
+    }
+
+    public double amount(){
+        return this.amount;
+    }
 }
