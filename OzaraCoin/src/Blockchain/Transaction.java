e759bd3053b5ab1880fb2bb482783e983bc6a344
Alexander-Muendesi <u20426918@tuks.co.za>
2023-04-05 22:00:35 +0200

    ✨ Added Code to check for a valid digital signature

--- OzaraCoin/src/Blockchain/Transaction.java
+++ OzaraCoin/src/Blockchain/Transaction.java
@@ -2,18 +2,27 @@ package Blockchain;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 //need to implement a way to sign a transaction with a digital transaction to 
 //ensure its authenticity
 public class Transaction {
-    private String sender;
-    private String receiver;
-    private double amount;
-    private double fee;
-    private String transactionId;
-    private String transactionSignature;
+    private final String sender;
+    private final String receiver;
+    private final double amount;
+    private final double fee;
+    private final String transactionId;
+    private final String transactionSignature;
+    private final PublicKey senderPublicKey;
 
-    public Transaction(String sender, String receiver, double amount,double fee, String transactionId,String transactionSignature){
+    public Transaction(String sender, String receiver, double amount,double fee, String transactionId,String transactionSignature, PublicKey senderPublicKey){
         this.sender = sender;
         this.receiver = receiver;
         this.amount = amount;
@@ -21,6 +30,8 @@ public class Transaction {
         this.fee = fee;
         this.transactionId = transactionId;
         this.transactionSignature = transactionSignature;
+
+        this.senderPublicKey = senderPublicKey;
     }
 
     public String getSender(){
@@ -52,9 +63,46 @@ public class Transaction {
         getFee() + " " + getTrasactionId() + " " + getTransactionSignature(); 
     }
 
+    /**
+     * This method checks whether the Digital signature for the transaction is valid using the following steps
+     * Step 1: Get the senders public Key
+     * Step 2: Compute hash of transaction data excluding the digital signature with sha256
+     * Step 3: Use public key to verify signature by decrypting the signature using the public key and comparing the result with the hash of the transaction data
+     *          If the decryped signature matches the hash, then the digital signature is considered valid
+     * @return
+     */
     public boolean isValidSignature(){
-        //TODO: implement code to validate signature
+        Sha256 sha256 = new Sha256();
+        String hash = sha256.hash(getData());
 
+        byte[] encryptedData = transactionSignature.getBytes();
+
+        try {
+            Cipher cipher = Cipher.getInstance("DSA");
+            cipher.init(Cipher.DECRYPT_MODE, senderPublicKey);
+
+            byte[] decryptedData = cipher.doFinal(encryptedData);
+            String result = new String(decryptedData);
+
+            if(!hash.equals(result))
+                return false;
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return false;
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            return false;
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+            return false;
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+            return false;
+        }
         return true;
     }
 
