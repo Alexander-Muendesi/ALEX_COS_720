e249682fadcdfc90ed3857a4f5512f15bb28b93c
Alexander-Muendesi <u20426918@tuks.co.za>
2023-04-10 12:58:57 +0200

    ✨ Added Validation of Transactions

--- OzaraCoin/src/Blockchain/Mempool.java
+++ OzaraCoin/src/Blockchain/Mempool.java
@@ -6,9 +6,11 @@ import java.util.List;
 
 public class Mempool {
     private List<Transaction> transactions;
+    private List<Block> blockchain;
 
-    public Mempool(){
+    public Mempool(List<Block> blockchain){
         this.transactions = new ArrayList<Transaction>();
+        this.blockchain = blockchain;
     }
 
     public void addTransaction(Transaction transaction){
@@ -24,6 +26,12 @@ public class Mempool {
         return this.transactions;
     }
 
+    /**
+     * This method checks whether a transaction is valid before it is entered into a mempool.
+     * It first checks whether the digital signature is valid. Then it checks whether the sender has enough funds to carry out the transaction
+     * @param transaction
+     * @return True if the transation is valid else false.
+     */
     private Boolean isValidTransaction(Transaction transaction){
         //check all required fields are present
         if(transaction.getSender() == null || transaction.getReceiver() == null || transaction.getAmount() <=0 || transaction.getFee() <= 0 || 
@@ -35,8 +43,29 @@ public class Mempool {
         if(!transaction.isValidSignature())
             return false;
 
+        //next we have to check whether the sender has enough funds to carry out the transaction. Need to implement tha UTXO class first though
+        double senderMoney = 0.0;
+        double senderSpentMoney = 0.0;
+
+        String senderAddress = transaction.getSender();
+        for(Block block: blockchain){
+            for(Transaction transactionTwo : block.getTransactions()){
+                if(senderAddress.equals(transactionTwo.getReceiver()))//find out how much money the sender has received
+                    senderMoney += transactionTwo.getAmount();
+                if(senderAddress.equals(transactionTwo.getSender()))//find out how much the sender has spent
+                    senderSpentMoney += transactionTwo.getAmount();
+            }
+        }
+
+        if(senderMoney - senderSpentMoney < transaction.getAmount())//check if the sender can afford to complete the transaction
+            return false;
+
         //check for double spending
-        //TODO: implement double spending logic
+        String transactionId = transaction.getTrasactionId();
+        for(Block block: blockchain)
+            for(Transaction transactionTwo: block.getTransactions())
+                if(transactionId.equals(transactionTwo.getTrasactionId()))
+                    return false;
 
         return true;
     }
